467e5742580c9ed7b0130b6d9a5f73c8bedfcb12
Francisco Wanzeller <franciscowanzeller@MBPdeFrancisco.home>
2014-10-25 14:52:57 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,6 +9,7 @@
     // your code here
     character = (Character*)[CCBReader load:@"Character"]; //load a new Character from the CCB file named Character.
     [physicsNode addChild:character]; //add your character as a child of physicsNode so that physics will be applied to it.
+    [self addObstacle];
 }
 
 -(void)update:(CCTime)delta
