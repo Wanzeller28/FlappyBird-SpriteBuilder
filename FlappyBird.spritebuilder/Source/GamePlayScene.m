c29cf5553ada9758c0fe2f95c309888ee3b42b6c
Francisco Wanzeller <franciscowanzeller@MBPdeFrancisco.home>
2014-10-25 15:48:29 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,24 @@
     character = (Character*)[CCBReader load:@"Character"]; //load a new Character from the CCB file named Character.
     [physicsNode addChild:character]; //add your character as a child of physicsNode so that physics will be applied to it.
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
--(void)update:(CCTime)delta
+-(void)update:(CCTime)delta //in Cocos2D the update loop runs 60 times per second by default!
 {
     // put update code here
+    // Increment the time since the last obstacle was added
+    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
+    
+    // Check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // Then reset the timer.
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
